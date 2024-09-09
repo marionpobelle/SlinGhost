46904ddf269932504f9ebc39f8dddb6aaa23f0fb
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-09-09 13:58:49 +0200

    Add MouseCrossHairProvider

--- Assets/Scripts/CrosshairController.cs
+++ Assets/Scripts/CrosshairController.cs
@@ -1,6 +1,15 @@
+using System;
 using UnityEngine;
 
+[DisallowMultipleComponent]
+[SelectionBase]
 public class CrosshairController : MonoBehaviour
 {
-        
+    public event Action<Vector3> OnSlingshotFired;
+
+    public void Fire()
+    {
+        Debug.Log("Slingshot fired at position: " + transform.position, this);
+        OnSlingshotFired?.Invoke(transform.position);
+    }
 }
---
+++ Assets/Scripts/MouseCrosshairProvider.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// Sets the position of the crosshair controller to the position of the mouse cursor on the plane.
+/// It only used for debugging purposes.
+/// </summary>
+public class MouseCrosshairProvider : MonoBehaviour
+{
+    private CrosshairController _crosshairController;
+    private Camera _mainCamera;
+
+    private void Start()
+    {
+        _crosshairController = FindObjectOfType<CrosshairController>();
+        if (!_crosshairController)
+        {
+            Debug.LogError("CrosshairController not found in the scene.", this);
+        }
+        _mainCamera = Camera.main;
+        if (!_mainCamera)
+        {
+            Debug.LogError("Main camera not found in the scene.", this);
+        }
+    }
+
+    private void Update()
+    {
+        if (!_crosshairController) return;
+        // Get the mouse position
+        var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hit))
+        {
+            // Set the crosshair position to the hit point
+            _crosshairController.transform.position = hit.point;
+        }
+    }
+}
