0b3ecaa581b4683166cdbeff03093b5eca157746
Tom D'export <71851742+tomdexp@users.noreply.github.com>
2024-09-09 14:18:04 +0200

    Add cooldown for CrosshairController

--- Assets/Scripts/CrosshairController.cs
+++ Assets/Scripts/CrosshairController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 [DisallowMultipleComponent]
@@ -6,10 +7,29 @@ using UnityEngine;
 public class CrosshairController : MonoBehaviour
 {
     public event Action<Vector3> OnSlingshotFired;
+    private bool _isInCooldown;
+    private WaitForSeconds _cooldownInSeconds;
+
+    private void Awake()
+    {
+        _cooldownInSeconds = new WaitForSeconds(Data.GameData.CooldownBetweenShotsInSeconds);
+    }
 
     public void Fire()
     {
+        if (_isInCooldown) return;
         Debug.Log("Slingshot fired at position: " + transform.position, this);
         OnSlingshotFired?.Invoke(transform.position);
+        StartCoroutine(CooldownCoroutine());
+    }
+    
+    private IEnumerator CooldownCoroutine()
+    {
+        if (_isInCooldown) yield break;
+        Debug.Log($"Slingshot is in cooldown, waiting {Data.GameData.CooldownBetweenShotsInSeconds} seconds...", this);
+        _isInCooldown = true;
+        yield return _cooldownInSeconds;
+        Debug.Log("Slingshot is ready to fire.", this);
+        _isInCooldown = false;
     }
 }
--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -7,3 +7,32 @@ public class GameData : ScriptableObject
     [Tooltip("The minimum time between shots.")]
     public float CooldownBetweenShotsInSeconds = 0.5f;
 }
+
+
+
+// Create a static getter that load GameData from Resources
+// Usage : Data.GameData (from anywhere in the project)
+public static class Data
+{
+    private static GameData _gameData;
+
+    static Data()
+    {
+        Application.quitting += OnApplicationQuit;
+    }
+
+    public static GameData GameData
+    {
+        get
+        {
+            if (!_gameData) _gameData = Resources.Load<GameData>(nameof(GameData));
+
+            return _gameData;
+        }
+    }
+
+    private static void OnApplicationQuit()
+    {
+        _gameData = null;
+    }
+}
