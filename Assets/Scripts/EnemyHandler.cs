b1ea248f73679e646e2dee39635b59f8e3be5816
POBELLE Marion <marion.pobelle.auditeur@lecnam.net>
2024-09-09 15:06:51 +0200

    Enemy + difficulty progression GameData

--- Assets/Scripts/EnemyHandler.cs
+++ Assets/Scripts/EnemyHandler.cs
@@ -1,28 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class EnemyHandler : MonoBehaviour
 {
+    [Header("REFERENCES")]
+    [SerializeField] private GameData _gameData;
+
     private CrosshairController _crosshairController;
+
+    //Contains the crosshair position updated the last time the slingshot was fired.
+    private UnityEngine.Vector3 _crosshairPosition;
+
+    private void Awake()
+    {
+        transform.localScale = new UnityEngine.Vector3(_gameData.EnemyMinScale, _gameData.EnemyMinScale, _gameData.EnemyMinScale);
+        _crosshairPosition = UnityEngine.Vector3.zero;
+    }
 
     private void Start()
     {
         _crosshairController = FindObjectOfType<CrosshairController>();
-        //_crosshairController.OnSlingshotFired += SlingshotFired;
+        _crosshairController.OnSlingshotFired += SlingshotFired;
     }
 
-    private void SlingshotFired()
+    private void SlingshotFired(UnityEngine.Vector3 crosshairPosition)
     {
+        _crosshairPosition = crosshairPosition;
         //IF ENEMY HIT
-        //Game won
+        //Stuff
         //IF ENEMY MISSED
         //Move enemy
         //Check if game is ended
     }
 
+    /// <summary>
+    /// Gets the distance in between the crosshair and the enemy on the Y axis.
+    /// If the distance is negative, the crosshair is above the enemy.
+    /// If the distance is positive, the crosshair is below the enemy.
+    /// </summary>
+    public float GetNormalizedYDistance()
+    {
+        UnityEngine.Vector3 differences = (this.transform.position - _crosshairPosition).normalized;
+        return differences.y;
+    }
+
+    /// <summary>
+    /// Gets the distance in between the crosshair and the enemy in 2D space.
+    /// </summary>
+    public float GetDistance()
+    {
+        return UnityEngine.Vector2.Distance(_crosshairPosition, this.transform.position);
+    }
+
     private void OnDestroy()
     {
-        //_crosshairController.OnSlingshotFired -= SlingshotFired;
+        _crosshairController.OnSlingshotFired -= SlingshotFired;
     }
 }
