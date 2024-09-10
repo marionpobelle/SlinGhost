9194a8903494f44602b39195a544131262a0f38b
POBELLE Marion <marion.pobelle.auditeur@lecnam.net>
2024-09-10 10:17:12 +0200

    GameData changes

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -27,6 +27,16 @@ public class GameData : ScriptableObject
     public float TimeInterval = 10f;
     [Tooltip("Amount by which the speed increases.")]
     public float Increase = 0.2f;
+
+    [Header("Spawner")]
+    [Tooltip("Max amount of enemies to spawn.")]
+    public int MaxAmountEnemies = 5;
+    [Tooltip("Amount of enemies to spawn per wave.")]
+    public int enemySpawnAmount = 1;
+    [Tooltip("Minimum time in between enemies spawning.")]
+    public float minSpawnTime = 10f;
+    [Tooltip("Maximum time in between enemies spawning.")]
+    public float maxSpawnTime = 15f;
 }
 
 
